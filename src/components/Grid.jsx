d87a19e57ed9890def0d37742351441e31994d36
Jonathan White <contact@jonathancwhite.com>
2023-03-21 15:13:24 -0500

    adding in portfolio grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,10 +1,399 @@
-import React from 'react'
+import React from 'react';
+import kodiakAgency from '../assets/img/kodiakagency.png';
+import creatorCarts from '../assets/img/creatorcarts.png';
+import ballisticAgency from '../assets/img/ballisticagency.png';
+import maxOrdGear from '../assets/img/maxordgear.png';
+import mileHigh from '../assets/img/milehigh.png';
+import lawrenceAmmo from '../assets/img/lawrenceammo.png';
+import mtCommodoreAme from '../assets/img/mtcommodoreame.png';
+import yhm from '../assets/img/yankeehill.png';
+import Image from 'next/image';
 
 const Grid = ({gridStyle}) => {
-  return (
-	<div className="grid">
-		<div className="grid__container">
 
+  return (
+	<div className="grid" id="grid__outerContainer">
+		<div className="row row__title" id="title-1">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Kodiak Agency</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={kodiakAgency} alt="Kodiak Agency Website Preview"/>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Ballistic Agency</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={ballisticAgency} alt=""/>
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Mile High Shooting</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={mileHigh} alt="" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Lawrence Ammo</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={lawrenceAmmo} alt="" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Max Ord</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={maxOrdGear} alt="" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Creator Carts</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={creatorCarts} alt=""/>
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">Mt. Commodore</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={mtCommodoreAme} alt="" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+					<div className="col__title">YHM MFG.</div>
+					<div className="col__title">Website</div>
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__work">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col filled">
+					<Image src={yhm} alt="" />
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
+		</div>
+		<div className="row row__title">
+			<div className="grid__row">
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+				<div className="col">
+
+				</div>
+			</div>
 		</div>
 	</div>
   )
