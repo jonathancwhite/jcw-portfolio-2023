b8f6276edf34fc6969dbe205ddaca778a8c803da
Jonathan White <contact@jonathancwhite.com>
2023-03-27 16:36:21 -0500

    fix some mobile styling and added mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,45 @@
 import Link from 'next/link'
-import React from 'react'
+import React, {useState} from 'react'
 import { useRouter } from 'next/router';
+import MenuIcon from '@mui/icons-material/Menu';
+import { MenuOpen } from '@mui/icons-material';
 
 const Header = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+	function openMenu() {
+		menuOpen ? setMenuOpen(false) : setMenuOpen(true);
+	}
   const router = useRouter();
   return (
-	<div className="header">
-		<div className="header__container">
-			<div className="logo-wrapper">
-				<Link href="/"><h2>jw.</h2></Link>
-			</div>
-			<div className="navmenu-wrapper">
-				<ul>
-					<li><Link href="/" className={router.pathname == "/" ? "active" : ""}>Projects</Link></li>
-					<li><Link href="/services" className={router.pathname == "/services" ? "active" : ""}>Services</Link></li>
-					<li><Link href="/about" className={router.pathname == "/about" ? "active" : ""}>About Me</Link></li>
-				</ul>
-			</div>
-			<div className="extramenu-wrapper">
-
+	<>
+		<div className="header">
+			<div className="header__container">
+				<div className="logo-wrapper">
+					<Link href="/"><h2>jw.</h2></Link>
+				</div>
+				<div className="navmenu-wrapper">
+					<ul>
+						<li><Link href="/" className={router.pathname == "/" ? "active" : ""}>Projects</Link></li>
+						<li><Link href="/services" className={router.pathname == "/services" ? "active" : ""}>Services</Link></li>
+						<li><Link href="/about" className={router.pathname == "/about" ? "active" : ""}>About Me</Link></li>
+					</ul>
+				</div>
+				<div className="mobileNav">
+					{
+						menuOpen ? <MenuOpen onClick={openMenu}/> : <MenuIcon onClick={openMenu} />
+					}
+				</div>
 			</div>
 		</div>
-	</div>
+		<div className={menuOpen ? 'mobileNav__menu mobile-open' : 'mobileNav__menu'}>
+			<div className="mobileNav__menu--items">
+				<Link href="/" className={router.pathname == "/" ? "active centered" : "centered"}>Projects</Link>
+				<Link href="/services" className={router.pathname == "/services" ? "active centered" : "centered"}>Services</Link>
+				<Link href="/about" className={router.pathname == "/about" ? "active centered" : "centered"}>About</Link>
+			</div>
+		</div>
+	</>
+
   )
 }
 
